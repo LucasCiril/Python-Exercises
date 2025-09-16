4846e7fbda4472c1f82dfa6d2e6a42501ca2e9a5
Lucas Cirilo <lucas.cirilo@escolar.ifrn.edu.br>
2025-09-16 17:11:14 -0300

    apenas atualizacao

--- Tratamento-de-dados/exercício3.py
+++
@@ -1,8 +0,0 @@
-#Crie um programa que leia dois números entre eles
-#e mostre a soma deles:
-
-n1 = float(input('Insira um número qualquer: '))
-n2 = float(input('Insira outro número qualquer: '))
-s = n1 + n2
-
-print(f'A soma entre {n1} e {n2} resulta em: {s}')
