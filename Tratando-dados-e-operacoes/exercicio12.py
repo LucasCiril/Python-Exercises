f6a90b0af54e190718947f56682c7080c53aa6dc
LucasCiril <lucas.cirilo@escolar.ifrn.edu.br>
2025-09-19 09:53:40 -0300

    update no código

--- Tratando-dados-e-operacoes/exercicio12.py
+++ Tratando-dados-e-operacoes/exercicio12.py
@@ -1,10 +1,10 @@
 #Faça um algoritmo que leia o preço de um produto
 #e mostre o seu novo preço, com 5% de desconto.
 
-def n_preço():
+def n_preço(n1):
     desc = (n1 * 5) / 100
     r_pre = n1 - desc
     print(f'O preço com desconto fica {r_pre:.02f}')
 
-n1 = float(input('Insira o preço original do produto: '))
-n_preço()
+b = float(input('Insira o preço original do produto: '))
+n_preço(b)
