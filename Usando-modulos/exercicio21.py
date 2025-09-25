b2e890eae37f88df913f915e06609bebc1667029
LucasCiril <lucas.cirilo@academico.ifrn.edu.br>
2025-09-25 15:21:32 -0300

    att exercicios

---
+++ Usando-modulos/exercicio21.py
@@ -0,0 +1,10 @@
+#Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+import time
+from pygame import mixer
+
+mixer.init()
+mixer.music.load('./Usando-modulos/Superman.mp3')
+mixer.music.play()
+
+while mixer.music.get_busy():
+    time.sleep(1)
